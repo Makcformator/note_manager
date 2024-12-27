c55f0ba887f30350e8bb4e475ca6e96e0bbc8e99
Makcformator <makcformator@gmail.com>
2024-12-27 23:09:18 +0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,17 @@
+username = 'Makc'  # Имя пользователя
+print(username)
+
+title = 'Name note'  # Заголовок заметки
+print(title)
+
+content = 'Text'  #  Описание заметки
+print(content)
+
+status = 'status'  # статус заметки
+print(status)
+
+created_date = "27-12-2024"  # дата создания заметки
+print(created_date)
+
+issue_date = "27-01-2024"  # дата истечения заметки
+print(issue_date)
