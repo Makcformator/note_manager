2834584992f5b29073b7080b610d2b0104218675
Makcformator <makcformator@gmail.com>
2025-01-08 22:41:01 +0400

    Add files via upload
    Grade 1. Этап 1: Задание 4

---
+++ add_list.py
@@ -0,0 +1,5 @@
+name_text1 = input('Основной заголовок: ')
+name_text2 = input("Дополнительный заголовок: ")
+name_text3 = input('Рекомендации к заметке: ')
+name_text = [name_text1, name_text2, name_text3]
+print(name_text)
