2233ffef38ba2becfa27d2fcb9c1cd3fe5f56ef5
Makcformator <makcformator@gmail.com>
2025-01-13 18:31:52 +0400

    Update add_list.py
    Модуль1, урок 4

--- add_list.py
+++ add_list.py
@@ -1,5 +1,2 @@
-name_text1 = input('Основной заголовок: ')
-name_text2 = input("Дополнительный заголовок: ")
-name_text3 = input('Рекомендации к заметке: ')
-name_text = [name_text1, name_text2, name_text3]
+name_text = [input('Основной заголовок: '), input("Дополнительный заголовок: "), input('Рекомендации к заметке: ')]
 print(name_text)
