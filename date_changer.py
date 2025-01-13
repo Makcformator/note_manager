e1700325d6ae073b6857aca0d0b5ec579fc5a532
Makcformator <makcformator@gmail.com>
2025-01-13 18:51:54 +0400

    Update date_changer.py
    Модуль 1, урок 2

--- date_changer.py
+++ date_changer.py
@@ -1,3 +1,4 @@
 created_date = input('Введите дату в формате "дд.мм.гггг: ')
 issue_date = input('Введите дату в формате "дд.мм.гггг: ')
-print(str(f'Дата: {data [0:5]}'))
+print(str(f'Дата: {created_date [0:5]}'))
+print(str(f'Дата: {issue_date [0:5]}'))
