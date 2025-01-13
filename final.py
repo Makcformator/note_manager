e01f0023b4dbd5f1b324be2c24af061eed5f7b94
Makcformator <makcformator@gmail.com>
2025-01-13 18:40:50 +0400

    Add files via upload
    Модуль 1, урок 5

---
+++ final.py
@@ -0,0 +1,11 @@
+username = input('Имя пользователя: ')
+created_date = input('Введите дату в формате "дд.мм.гггг: ')  # дата создания заметки
+issue_date = input('Введите дату в формате "дд.мм.гггг: ')  # дата истечения заметки
+title = input('Заголовок заметки: ')
+content = input('Описание: ')
+status = input("Статус")
+name_text1 = input('Основной заголовок: ')
+name_text2 = input("Дополнительный заголовок: ")
+name_text1 = input('Рекомендации к заметке: ')]
+note = [username, created_date[0:5], issue_date[0:5], title, content, [name_text1, name_text2, name_text3]]
+print(note)
