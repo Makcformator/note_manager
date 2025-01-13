f50ccd8fd2a50e322d0e957120523dae6f0dca92
Makcformator <makcformator@gmail.com>
2025-01-13 18:34:57 +0400

    Update greetings.py
    Модуль 1, урок 1

--- greetings.py
+++ greetings.py
@@ -1,17 +1,12 @@
 username = 'Makc'  # Имя пользователя
-print(username)
-
 title = 'Name note'  # Заголовок заметки
-print(title)
-
 content = 'Text'  #  Описание заметки
-print(content)
-
 status = 'status'  # статус заметки
-print(status)
-
 created_date = "27-12-2024"  # дата создания заметки
-print(created_date)
-
 issue_date = "27-01-2024"  # дата истечения заметки
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
 print(issue_date)
