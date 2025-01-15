927d9f4a3bb144754b2a7d2ff18af5a29bb8d7c4
Makcformator <makcformator@gmail.com>
2025-01-15 19:38:14 +0400

    Update add_input.py

--- add_input.py
+++ add_input.py
@@ -1,4 +1,8 @@
+created_date = input('Введите сегодняшнюю дату в формате "дд.мм.гггг: ')  # дата создания заметки
+issue_date = input('Введите срок даты в формате "дд.мм.гггг: ')  # дата истечения заметки
 title = input('Заголовок заметки: ')
 text = input('Описание: ')
+print(f'Дата: {created_date [0:5]}')
+print(f'Срок: {issue_date [0:5]}')
 print(title)
 print(text)
